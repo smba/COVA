0d89f40e46986f8ae4c13c220aea6e0894ba7169
Stefan Mühlbauer <muehlbauer@informatik.uni-leipzig.de>
2020-09-16 11:33:16 +0200

    dies das

---
+++ myTestBench/bench/Configuration.java
@@ -0,0 +1,16 @@
+package bench;
+
+import java.lang.Math;
+
+public class Configuration {
+	
+    public static boolean getX() {
+        return false;
+    }
+    
+    public static boolean getY() {
+        return true;
+    }
+	
+}
+ 
